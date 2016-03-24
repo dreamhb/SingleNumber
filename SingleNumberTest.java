be98e729fa7879d564f08531710ee225a9410af3
Jayden.hu <bing.hu1989@gmail.com>
2016-03-24 11:09:33 +0800

    using gradle for compile and test

--- SingleNumberTest.java
+++ SingleNumberTest.java
@@ -8,5 +8,7 @@ public class SingleNumberTest{
         SingleNumber sn = new SingleNumber();
         int[] x = {2, 1, 3, 5, 4, 6, 7, 9, 8, 10, 6, 11};
         assertEquals(6, sn.xorSingleNumber(x));
+        assertEquals(6, sn.minusSingleNumber(x));
+        assertEquals(6, sn.bitmapSingleNumber(x));
     }
 }
