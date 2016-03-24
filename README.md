322a2a263d9cbad52a2e5b385052862b8439f022
Jayden.hu <bing.hu1989@gmail.com>
2016-03-24 09:52:36 +0800

    add JUnit test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 #在数组中找出唯一重复的数值
+加入Junit测试
+
+参考链接：
+
+<https://github.com/junit-team/junit4/wiki/Getting-started>
 
---
+++ SingleNumberTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SingleNumberTest{
+    
+    @Test
+    public void evaluteExpression(){
+        SingleNumber sn = new SingleNumber();
+        int[] x = {2, 1, 3, 5, 4, 6, 7, 9, 8, 10, 6, 11};
+        assertEquals(6, sn.xorSingleNumber(x));
+    }
+}
